2077d977a264780cc46ab7a64434f9228632fb3d
AndreaGiovannaTapiaLira <20253rd013@utez.edu.mx>
2026-02-24 19:07:53 -0600

    Se coloca la informacion de la Hija

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main{
         Vehicul carro = new Vehicul("Hennessey", "Venom F5", "484 - 499 km/h");
         carro.mostrarinformacion();
         System.out.println("--------------------------------");
+        HijaVehicul hijo = new HijaVehicul("Bugatti", "Chiron Super Sport", "440 km/h (limitada)", "8.0 Litros (8000 cc)");
+        hijo.mostrarinformacion();
     }
 }
