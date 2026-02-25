033c87212a09ef7846d93eb3d6fcf9fd6c813c75
AndreaGiovannaTapiaLira <20253rd013@utez.edu.mx>
2026-02-24 18:59:50 -0600

    Se usa el override y se añade el atributo de la Hija y usando el extends para enlazar con el Vehiculo principal

--- HijaVehicul.java
+++ HijaVehicul.java
@@ -1,14 +1,23 @@
-public class HijaVehicul extends Vehicul{
+public class HijaVehicul extends Vehicul {
     // Atributo para la Hija
     protected String cilindrada;
 
     // Constructor de la Hija
-    public HijaVehicul(String marca, String modelo, String velmax, String cilidrada){
+    public HijaVehicul(String marca, String modelo, String velmax, String cilidrada) {
 
         // Llamar al constructor del Vehicul
-        super(marca,modelo,velmax);
+        super(marca, modelo, velmax);
 
         // Constructor de la Hija
         this.cilindrada = cilindrada;
+    }
+
+    // Polimorfismo
+    @Override
+    public void mostrarinformacion() {
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Velocidad Máxima: " + this.velmax);
+        System.out.println("Cilindrara: " + this.cilindrada);
     }
 }
