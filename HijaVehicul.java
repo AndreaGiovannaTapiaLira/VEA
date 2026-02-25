e31616b3167fc0f8e70a89f8addb77a7f047c3f5
AndreaGiovannaTapiaLira <20253rd013@utez.edu.mx>
2026-02-24 18:56:17 -0600

    Se crea el constructor y se manda a llamar del Vehiculo principal

--- HijaVehicul.java
+++ HijaVehicul.java
@@ -1,4 +1,14 @@
-public class HijaVehicul {
+public class HijaVehicul extends Vehicul{
     // Atributo para la Hija
     protected String cilindrada;
+
+    // Constructor de la Hija
+    public HijaVehicul(String marca, String modelo, String velmax, String cilidrada){
+
+        // Llamar al constructor del Vehicul
+        super(marca,modelo,velmax);
+
+        // Constructor de la Hija
+        this.cilindrada = cilindrada;
+    }
 }
